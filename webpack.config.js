a47c53b5145a1fe0bb0bca9b3ba5e8a6aeb1e134
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-11-20 00:08:05 +0900

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
 const path = require("path");
 
+console.log(__dirname);
+// /Users/yuu/Desktop/webpack
+console.log(path.resolve(__dirname, "dist"));
+console.log(path.join(__dirname, "dist"));
+// /Users/yuu/Desktop/webpack/dist
+console.log("---------------------");
+
 module.exports = {
+  // --mode=developmentを省略できるようになる
+  mode: "development",
   // 元になるファイル(中心)
   entry: "./src/index.js",
   // どこにbundleした後のファイルを置くか
   output: {
     // プロジェクト直下のdistというディレクトリ(なければ作る)
-    path: path.resolve(__dirname, "dist"),
+    path: path.join(__dirname, "dist"),
     // dist/bundle.js
     filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
   },
 };
